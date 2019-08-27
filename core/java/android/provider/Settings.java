0b4c33219f97718e4df787a3ff9e249b36280891
Sajidjnl <syedsajid341@gmail.com>
2019-08-27 09:23:01 +0000

    settings: remove duplicates

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5072,11 +5072,6 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String SMART_CHARGING_RESET_STATS = "smart_charging_reset_stats";
-
-        /**
-         * @hide
-         */
         public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
 
         /**
